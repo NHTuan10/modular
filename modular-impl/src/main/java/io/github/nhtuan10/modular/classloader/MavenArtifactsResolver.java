cdad17edec6271f1889d4dfe7815491afb506fc3
tuan <nhtuan10@gmail.com>
2025-06-28 16:54:19 -0400

    maven source and javadoc plugins for all sub-modules

--- modular-impl/src/main/java/io/github/nhtuan10/modular/classloader/MavenArtifactsResolver.java
+++ modular-impl/src/main/java/io/github/nhtuan10/modular/classloader/MavenArtifactsResolver.java
@@ -1,22 +1,23 @@
 package io.github.nhtuan10.modular.classloader;
 
+import lombok.extern.slf4j.Slf4j;
 import org.jboss.shrinkwrap.resolver.api.maven.Maven;
-
 import java.util.List;
 
+@Slf4j
 public class MavenArtifactsResolver<T> implements ArtifactsResolver<T> {
 
     public  List<T> resolveMavenDeps(List<String> deps, Class<T> clazz) {
 //        Maven.configureResolver().withRemoteRepo("central", "https://repo.maven.apache.org/maven2/", "default")
 //                .withMavenCentralRepo(true)
 //                .resolve();
-        List<T> dependencyLocation = Maven.resolver()
+        List<T> dependencyLocations = Maven.resolver()
 //                .resolve("org.apache.commons:commons-lang3:3.9")
 //                .resolve("org.eclipse.aether:aether-transport-http:1.1.0")
                 .resolve(deps)
                 .withTransitivity().asList(clazz);
 
-        System.out.println(dependencyLocation);
-        return dependencyLocation;
+        log.debug("Dependency locations for {} : {}", deps, dependencyLocations);
+        return dependencyLocations;
     }
 }
