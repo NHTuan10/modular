90622f44a4ca4e7e47724f25cb79563eab30feb7
tuan <nhtuan10@gmail.com>
2025-07-06 11:55:31 -0400

    test creating objects in Kryo or ByteBuddy that don't have no-arg constructors

--- modular-sample-api/src/main/java/io/github/nhtuan10/sample/api/service/SomeData.java
+++ modular-sample-api/src/main/java/io/github/nhtuan10/sample/api/service/SomeData.java
@@ -2,11 +2,10 @@ package io.github.nhtuan10.sample.api.service;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
-@NoArgsConstructor
+//@NoArgsConstructor
 public class SomeData {
     private String name;
 }
